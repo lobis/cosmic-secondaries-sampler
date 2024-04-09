7a406209a5863668fb659b1f55ef785638ccb28c
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2024-04-09 15:44:49 +0200

    it works!

--- sampling.C
+++ sampling.C
@@ -5,23 +5,24 @@
 #include <TH2D.h>
 #include <TF1.h>
 #include <TCanvas.h>
+#include <TRandom3.h>
 
 using namespace std;
 
 double diskToSphereRatio = 10.0;
 
-#include <TRandom3.h>
-
 TRandom3 *rnd = new TRandom3(0);
 
-TVector2 PointOnDisk() {
+TVector2 PointOnDisk()
+{
     double r = TMath::Sqrt(rnd->Uniform());
     double theta = rnd->Uniform() * 2 * TMath::Pi();
     return TVector2(r * TMath::Cos(theta), r * TMath::Sin(theta));
 }
 
 pair<bool, TVector3>
-IntersectionLineSphere(const TVector3 &lineOrigin, const TVector3 &lineDirection) {
+IntersectionLineSphere(const TVector3 &lineOrigin, const TVector3 &lineDirection)
+{
     // sphere origin is always (0,0)
     // return the first intersection point
     // https://en.wikipedia.org/wiki/Line%E2%80%93sphere_intersection
@@ -30,7 +31,8 @@ IntersectionLineSphere(const TVector3 &lineOrigin, const TVector3 &lineDirection
     const double c = lineOrigin.Dot(lineOrigin) - 1;
 
     const double discriminant = b * b - 4 * a * c;
-    if (discriminant < 0) {
+    if (discriminant < 0)
+    {
         return {false, {0, 0, 0}};
     }
     const double t1 = (-b + sqrt(discriminant)) / (2 * a);
@@ -42,13 +44,19 @@ IntersectionLineSphere(const TVector3 &lineOrigin, const TVector3 &lineDirection
 TF1 zenithAngle("zenithAngle", "TMath::Power(TMath::Cos(x), 2)", 0, TMath::Pi() / 2.0);
 TF1 zenithAngleCorrected("zenithAngleCorrected", "zenithAngle(x) / TMath::Cos(x)", 0, TMath::Pi() / 2.0);
 
-void sampling() {
+void sampling()
+{
     TH1D *zenithHist = new TH1D("Zenith", "Zenith", 100, 0, TMath::Pi() / 2.0);
     TH1D *zenithCompareHist = new TH1D("Zenith Compare", "Zenith Compare", 100, 0, TMath::Pi() / 2.0);
     TH2D *positionInDisk = new TH2D("Position in Disk", "Position in Disk", 1000, -10, 10, 1000, -10, 10);
     TH1D *distanceToCenter = new TH1D("distanceToCenter", "distanceToCenter", 1000, 0, 5);
-    while (zenithHist->GetEntries() < 10000000) {
+    while (zenithHist->GetEntries() < 1000000)
+    {
         break;
+        if (int(zenithHist->GetEntries()) % 10000 == 0)
+        {
+            cout << zenithHist->GetEntries() << endl;
+        }
         const TVector2 pointOnDisk = PointOnDisk() * diskToSphereRatio;
         const TVector3 point(pointOnDisk.X(), 1, pointOnDisk.Y());
 
@@ -58,9 +66,9 @@ void sampling() {
         const TVector3 direction = {TMath::Sin(zenith) * TMath::Cos(phi), -1.0 * TMath::Cos(zenith),
                                     TMath::Sin(zenith) * TMath::Sin(phi)};
 
-
         const auto [intersectionFlag, intersection] = IntersectionLineSphere(point, direction);
-        if (!intersectionFlag) {
+        if (!intersectionFlag)
+        {
             continue;
         }
         positionInDisk->Fill(pointOnDisk.X(), pointOnDisk.Y());
@@ -69,7 +77,8 @@ void sampling() {
         zenithCompareHist->Fill(zenithAngleCorrected.GetRandom());
     }
 
-    while (zenithHist->GetEntries() < 10000000) {
+    while (zenithHist->GetEntries() < 10000000)
+    {
         // break;
         const auto zenith = zenithAngleCorrected.GetRandom();
         // ellipse center is (tan(zenith), 0) and major axis is 1/cos(zenith), minor axis is 1
@@ -79,7 +88,8 @@ void sampling() {
         double y = 1;
         const double xOffset = TMath::Tan(zenith);
         double x = 1 / TMath::Cos(zenith) + xOffset;
-        while (TMath::Power(x - TMath::Sin(zenith), 2) * TMath::Cos(zenith) + TMath::Power(y, 2) > 1) {
+        while (TMath::Power((x - xOffset) * TMath::Cos(zenith), 2) + TMath::Power(y, 2) > 1)
+        {
             y = rnd->Uniform(-1, 1);
             x = rnd->Uniform(-1, 1) * 1 / TMath::Cos(zenith) + xOffset;
         }
@@ -106,9 +116,7 @@ void sampling() {
     // zenithHist->SetLineColor(kRed);
     // zenithHist->Draw();
 
-
     // draw zenithAngle and zenithAngleCorrected on top
     // zenithAngle.SetLineColor(kGreen);
     // zenithCompareHist->Draw("same");
-
 }
